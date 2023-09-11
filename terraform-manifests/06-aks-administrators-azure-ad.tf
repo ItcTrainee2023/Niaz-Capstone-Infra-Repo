24cd861ac714aeb910aeb5e48b7a9524d189ae2c
CapstoneProjectItc <140419054+ItcTrainee2023@users.noreply.github.com>
2023-09-11 23:33:20 +0100

    1st Commit

---
+++ terraform-manifests/06-aks-administrators-azure-ad.tf
@@ -0,0 +1,10 @@
+# Create Azure AD Group in Active Directory for AKS Admins
+resource "azuread_group" "aks_administrators" {
+  #name        = "${azurerm_resource_group.aks_rg.name}-administrators"
+  display_name        = "capstone-aks-admins-niaz"
+  description = "Azure AKS Kubernetes administrators for the ${azurerm_resource_group.aks_rg.name}-${var.environment} cluster."
+  security_enabled = true
+}
+data "azuread_group" "capstone-aks-admins"{
+  name = "capstone-aks-admins-niaz"
+}
